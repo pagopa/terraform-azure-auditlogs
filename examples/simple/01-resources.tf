342ac4a63e0e5d29dbc2f08c814bc717b481629a
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2024-07-01 16:42:48 +0200

    removed unused variables

--- examples/simple/01-resources.tf
+++ examples/simple/01-resources.tf
@@ -9,26 +9,16 @@ module "azure_auditlogs" {
   source                  = "../.."
   resource_group_name     = azurerm_resource_group.rg.name
   location                = var.location
-  storage_account_name    = replace("${local.project}st", "-", "")
-  storage_container_name  = "auditlogs"
-  eventhub_namespace_name = "${local.project}-evhns"
-  appi_name               = "${local.project}-appi"
-  eventhub_name           = "${local.project}-evh"
-  capacity                = 4
-  law_name                = "${local.project}-law"
-  auto_inflate            = var.auto_inflate
+  storage_account         = {name = replace("${local.project}st", "-", "")}
+  event_hub               = {namespace_name ="${local.project}-evhns", capacity = 4, auto_inflate_enabled = var.auto_inflate_enabled}
+  application_insights    = {name = "${local.project}-appi"}
+  log_analytics_workspace = {name = "${local.project}-law"}
   account_replication     = var.account_replication
   account_tier            = var.account_tier
   access_tier             = var.access_tier
   export_rule_name        = "${local.project}-exp"
   table_names             = var.table_names
   tags                    = var.tags
-  stream_job_name         = "${local.project}-job"
-  file_path               = "../../transformation_query.sql"
-  cluster_name            = "${local.project}-dec"
-  cluster_sku = {
-    capacity = 2,
-    name     = "Standard_E2a_v4"
-  }
-  db_name = "${local.project}-dec-dedb"
+  stream_analytics_job    = {name = "${local.project}-job"}
+  data_explorer           = {name = "${local.project}-dec", sku_name = var.sku_name, sku_capacity = var.sku_capacity} 
 }
--- variables.tf
+++ variables.tf
@@ -3,81 +3,10 @@ variable "location" {
   description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
 }
 
-variable "law_name" {
-  type        = string
-  description = "Specifies the name of the Log Analytics Workspace. Changing this forces a new resource to be created."
-}
-
 // Resource Group
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group "
-}
-
-variable "law_sku" {
-  type        = string
-  description = "Specifies the SKU for the Log Analytics Workspace"
-  default     = "PerGB2018"
-}
-
-variable "appi_name" {
-  type        = string
-  description = "Specifies the name for the Application Insight"
-}
-
-variable "capacity" {
-  type        = number
-  description = "Specifies the Throughput unit for the Event Hub"
-}
-
-variable "eventhub_name" {
-  type        = string
-  description = "Specifies the EventHub Name"
-}
-
-variable "storage_account_name" {
-  type        = string
-  description = "Specifies the storage account name"
-}
-
-variable "eventhub_namespace_name" {
-  type        = string
-  description = "Specifies the Eventhub namespace name"
-}
-
-variable "storage_container_name" {
-  type        = string
-  description = "Specifies the storage account container name"
-}
-
-variable "auto_inflate" {
-  type        = bool
-  description = "Specifies if the autoscale is enabled or not for the Event hub Namespace"
-}
-
-variable "account_replication" {
-  type        = string
-  description = "Specifies the replication for the Storage account"
-}
-
-variable "account_tier" {
-  type        = string
-  description = "Specifies the account tier for the Storage account"
-}
-
-variable "access_tier" {
-  type        = string
-  description = "Specifies the access tier for the Storage account"
-}
-
-variable "export_rule_name" {
-  type        = string
-  description = "Specifies the name for the export rule"
-}
-
-variable "table_names" {
-  type        = list(string)
-  description = "Specifies the Table to be exported"
 }
 
 variable "application_insights" {
@@ -133,14 +62,6 @@ variable "stream_analytics_job" {
     streaming_units      = number,
     transformation_query = optional(string, "transformation_query.sql"),
   })
-}
-
-variable "stream_job_name" {
-  type = string
-}
-
-variable "file_path" {
-  type = string
 }
 
 variable "data_explorer" {
