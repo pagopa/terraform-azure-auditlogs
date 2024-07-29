bfa5f0f2e4774286bef5d21091ccb3cfa57ae379
pasqualedevita <>
2024-07-29 11:49:53 +0200

    fix path

--- external_table.sql
+++ external_table.sql
@@ -4,6 +4,6 @@ partition by (['IngestionDatetime']:datetime )
 pathformat = (datetime_pattern("yyyy/MM/dd/HH/mm", ['IngestionDatetime']))
     dataformat = multijson
     (
-        h@'https://${storage_account_name}.blob.core.windows.net/${storage_account_container_name}/record;impersonate'
+        h@'https://${storage_account_name}.blob.core.windows.net/${storage_account_container_name}/audit-logs;impersonate'
     )
     with (FileExtension=json)
