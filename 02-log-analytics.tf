a60246fa666aa59bc74fc651621d518f28fd28b7
re_sh_cloud_arch <94049389+re-sh-cloud-arch@users.noreply.github.com>
2026-01-14 12:40:22 +0100

    fix: Paymcloud 633 auditlogs to tf4 fix (#10)

--- 02-log-analytics.tf
+++ 02-log-analytics.tf
@@ -2,7 +2,7 @@ resource "azurerm_log_analytics_data_export_rule" "this" {
   for_each = toset(var.log_analytics_workspace.export_tables)
 
   name                    = each.value
-  resource_group_name     = var.resource_group_name
+  resource_group_name     = var.log_analytics_workspace_rg_name
   workspace_resource_id   = var.log_analytics_workspace.id
   destination_resource_id = azurerm_eventhub.law.id
   table_names             = [each.value]
